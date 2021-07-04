a89049ac7e8c50ea3075b9bec2f432f5db431a3d
titaniume2 <titaniume@163.com>
2021-07-04 19:55:34 +0800

    服务发现discovery

--- cloud-provider-payment8001/src/main/java/com/titaniume/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/titaniume/springcloud/controller/PaymentController.java
@@ -4,9 +4,13 @@ import com.titaniume.springcloud.entities.CommonResult;
 import com.titaniume.springcloud.entities.Payment;
 import com.titaniume.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @program: cloud2021
@@ -21,13 +25,19 @@ public class PaymentController
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody  Payment payment){
         int result = paymentService.create(payment);
         log.info("******插入结果******"+result);
         if(result > 0){
-            return  new CommonResult(200,"插入成功",result);
+            return  new CommonResult(200,"插入成功,serverPort:"+serverPort,result);
         }else{
             return  new CommonResult(200,"插入成功",null);
         }
@@ -39,11 +49,34 @@ public class PaymentController
         Payment payment= paymentService.getPaymentById(id);
         log.info("******查询结果******"+payment);
         if(payment!=null){
-            return  new CommonResult(200,"查询成功",payment);
+            return  new CommonResult(200,"查询成功,serverPort:"+serverPort,payment);
         }else{
             return  new CommonResult(200,"没有对应记录，查询ID"+id,null);
         }
     }
+
+
+
+    /*** 
+     * @description: 对于注册进eureka里面的微服务，可以通过服务发现来获得该服务的信息
+     * @return Object
+     * @author: renfei.huang
+     * @date: 2021-07-04  19:50
+     */
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
+
 
 
 }
