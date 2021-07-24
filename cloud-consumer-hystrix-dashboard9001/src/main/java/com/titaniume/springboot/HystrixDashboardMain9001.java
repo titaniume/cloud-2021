9337bbb856ca85171ce7a6a3253ce6793e7e7c43
titaniume2 <titaniume@163.com>
2021-07-25 00:26:51 +0800

    服务监控hystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/titaniume/springboot/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.titaniume.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: renfei.huang
+ * @create: 2021-07-25 00:13
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
