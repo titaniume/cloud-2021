ae64ec89f83ed94e91a49bc042f99d8b332bdff1
titaniume2 <titaniume@163.com>
2021-07-24 12:23:46 +0800

    hystrix案例 8001 服务不可以了 给服务降级 有一个兜底处理方案  比如制造异常  1.接收3秒 运行5秒超时异常  2.int age = 1/0 计算异常

--- cloud-provider-hystrix-payment8001/src/main/java/com/titaniume/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/titaniume/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.titaniume.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -20,13 +22,18 @@ public class PaymentService {
     }
 
 
-    //失败
+   @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+           @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+   })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
-        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
-        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+//        int timeNumber = 5;
+        int age = 10 / 0 ;
+        try { TimeUnit.SECONDS.sleep(3000); }catch (Exception e) {e.printStackTrace();}
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：   "+id+"\t"+"哈哈哈"  ;
     }
 
-
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"8001系统繁忙或者运行报错，请稍后再试 id："+id+"\t"+"o(╥﹏╥)o";
+    }
 
 }
