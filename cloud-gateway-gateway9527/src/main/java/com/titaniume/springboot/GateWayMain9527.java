1d5ace3117f402a22964655a3cda80bf383ebbe9
titaniume2 <titaniume@163.com>
2021-07-25 22:41:24 +0800

    gateway 配置路由yml方式

---
+++ cloud-gateway-gateway9527/src/main/java/com/titaniume/springboot/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.titaniume.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: renfei.huang
+ * @create: 2021-07-25 22:12
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
