f49864608a2881b71237093f3c92944b8d0cdcb2
titaniume2 <titaniume@163.com>
2021-07-27 00:23:31 +0800

    gateway 注入RouteLocator的Bean方式

---
+++ cloud-gateway-gateway9527/src/main/java/com/titaniume/springboot/config/GateWayConfig.java
@@ -0,0 +1,41 @@
+package com.titaniume.springboot.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: cloud2021
+ * @description: 代码中注入RouteLocator的Bean
+ * @author: renfei.huang
+ * @create: 2021-07-25 22:41
+ */
+@Configuration
+public class GateWayConfig {
+
+    /**
+     * 配置一个id为route-name的路由规则，
+     * 当访问地址http://localhost:9527/guonei时会自动转发到地址：http://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_titaniume",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_titaniume2",
+                r -> r.path("/guoji")
+                        .uri("http://news.baidu.com/guoji")).build();
+        return routes.build();
+    }
+
+}
