109f22c9f8a7ff91704e93d40797a47488c6eff0
titaniume2 <titaniume@163.com>
2021-07-28 21:51:05 +0800

    gateway Predicate就是为了实现一组匹配规则，让请求过来找到对应的Route进行处理

---
+++ cloud-provider-payment8002/src/test/java/Test.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: renfei.huang
+ * @create: 2021-07-28 21:29
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
+        System.out.println(zbj);
+
+    }
+}
