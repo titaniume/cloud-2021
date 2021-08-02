39b2a16191d460eae7a9b8d4ed3f5eb00c0cccc9
titaniume2 <titaniume@163.com>
2021-08-02 22:10:18 +0800

    SpringCloud Stream消息驱动 消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/titaniume/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.titaniume.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: renfei.huang
+ * @create: 2021-08-02 21:50
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
