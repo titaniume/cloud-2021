1cfd85faee71bd825754c7661c6ce12bd943513b
titaniume2 <titaniume@163.com>
2021-08-28 16:01:32 +0800

    SpringCloud Alibaba Sentinel 降级规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/titaniume/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/titaniume/springcloud/controller/FlowLimitController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: cloud2021
  * @description:
@@ -24,4 +26,28 @@ public class FlowLimitController {
         log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "------------testB";
     }
+
+
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+
+        log.info("testD 异常比例");
+        int age = 10/0;
+        return "------testD";
+    }
+
+
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
 }
